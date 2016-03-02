172c48e8f84391604e486d123a99f3d6870bd8ce
323142133 <323142133@C-5Q3ZJS1.tdsb.on.ca>
2016-03-02 14:44:51 -0500

    Created Hello World, first commit to github

---
+++ ICS3U/src/stanfield/HelloWorld.java
@@ -0,0 +1,14 @@
+package stanfield;
+/*HellowWorld.java
+ * This program prints Hello World to the console
+ * Brennan Stanfield
+ * Mar/2/2016 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Print "Hello World" in the console
+		System.out.println("Hello World");
+	}
+
+}
