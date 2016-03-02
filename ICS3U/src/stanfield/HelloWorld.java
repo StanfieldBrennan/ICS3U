dc545cc55898b44bd64c468af8eb6b6fa321c2fb
323142133 <323142133@C-5Q3ZJS1.tdsb.on.ca>
2016-03-02 14:57:57 -0500

    I added "Goodbye World" to show in the console

--- ICS3U/src/stanfield/HelloWorld.java
+++ ICS3U/src/stanfield/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Print "Hello World" in the console
 		System.out.println("Hello World");
+		System.out.print("Goodbye World");
 	}
 
 }
