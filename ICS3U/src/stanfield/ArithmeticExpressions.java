cacc166813decc4800336a67cc57fe8fb50862a1
323142133 <323142133@C-5Q3ZJS1.tdsb.on.ca>
2016-03-10 15:04:25 -0500

    Got to the processing point of the program

---
+++ ICS3U/src/stanfield/ArithmeticExpressions.java
@@ -0,0 +1,40 @@
+/*ArithmeticExpressions.java
+ * This program takes 5 numbers inputed by the user and outputs different things using the numbers
+ * Brennan Stanfield
+ * March 10 2016 
+ */ 
+
+package stanfield;
+
+import java.util.Scanner;
+
+public class ArithmeticExpressions {
+
+	public static void main(String[] args) {
+	
+		Scanner myInput = new Scanner(System.in); 
+		
+		//Declaring Variables 
+		int numOne;
+		int numTwo;
+		int numThree;
+		int numFour;
+		int numFive;
+		
+		//Input
+		System.out.println("Enter the first number");
+		numOne = Integer.parseInt(myInput.nextLine());
+		System.out.println("Enter the second number");
+		numTwo = Integer.parseInt(myInput.nextLine());
+		System.out.println("Enter the third number");
+		numThree = Integer.parseInt(myInput.nextLine());
+		System.out.println("Enter the fourth number");
+		numFour = Integer.parseInt(myInput.nextLine());
+		System.out.println("Enter the fifth number");
+		numFive = Integer.parseInt(myInput.nextLine());
+		
+		//Processing
+
+	}
+
+}
