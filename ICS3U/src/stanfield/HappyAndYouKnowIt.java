e9cfee2f3236c2100970af89f8d72795f3c6543f
323142133 <323142133@C-5Q3ZJS1.tdsb.on.ca>
2016-05-10 14:30:26 -0400

    done

---
+++ ICS3U/src/stanfield/HappyAndYouKnowIt.java
@@ -0,0 +1,25 @@
+/*HappyAndYouKnowIt.java
+ * 10/05/16
+ * Brennan Stanfield
+ * Prints the lyrics to your happy and you know it with the actions stored in an array
+ */
+package stanfield;
+
+public class HappyAndYouKnowIt {
+
+	public static void main(String[] args) {
+
+		String [] actions = new String[]{"clap your hands", "clap your fingers", "and you really really want to show it", "clap your feet", "stomp your feet", "snap your fingers", "and you really want to show it", "stomp one foot", "stomp the other foot", "jump around!"};
+		
+		for (int i = 0; i!=10; i++){
+			System.out.println("If you're happy and you know it " + actions[i]);
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
