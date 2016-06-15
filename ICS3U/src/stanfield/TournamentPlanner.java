e63fa81510107e27d96ea9d9a32c8b4ad5438021
323142133 <323142133@C-5Q3ZJS1.tdsb.on.ca>
2016-06-15 14:35:56 -0400

    almost there

--- ICS3U/src/stanfield/TournamentPlanner.java
+++ ICS3U/src/stanfield/TournamentPlanner.java
@@ -5,6 +5,7 @@
  */
 package stanfield;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TournamentPlanner {
@@ -15,59 +16,69 @@ public class TournamentPlanner {
 
 		Scanner scan = new Scanner(System.in);
 
-		//do while to make sure that the number of teams participating is even
-			do{
-				//asks user how many teams will be participating
+		// do while to make sure that the number of teams participating is even
+		boolean error = false;
+		do {
+			// asks user how many teams will be participating
 			System.out.println("How many teams are in the round robin? Please ensure it is an even number.");
-			amountTeams = Integer.parseInt(scan.nextLine());
-			}while(!isDivisible(amountTeams, 2));
 
-		//string to hold team names
-		String [] teamNames = new String[amountTeams];
+			try {
+				amountTeams = amountTeams / 2;
+				amountTeams = Integer.parseInt(scan.nextLine());
+				error = false;
+			} catch (NumberFormatException e) {
+				error = true;
+			}
 
-		//asks what the team names are
+		} while (!isDivisible(amountTeams, 2) || error == true);
+
+		// string to hold team names
+		String[] teamNames = new String[amountTeams];
+
+		// asks what the team names are
 		System.out.println("What are the team names?");
-		//repeats the output and stores input for as many times as needed
-		for (int i = 0; i<amountTeams; i++){
+		// repeats the output and stores input for as many times as needed
+		for (int i = 0; i < amountTeams; i++) {
 			teamNames[i] = scan.nextLine();
 		}
-		
-		//out puts how many matches will be needed
+
+		// out puts how many matches will be needed
 		System.out.println("You will need " + calculation(amountTeams) + " matches.");
-		
-		//prints the match ups of teams
+
+		// prints the match ups of teams
 		String[][] matchups = matchUps(teamNames);
 
-		for ( int i =0 ; i < matchups[0].length; i++){
+		for (int i = 0; i < matchups[0].length; i++) {
 			System.out.print(matchups[0][i] + " ");
 			System.out.println(matchups[1][i]);
 		}
 
 	}
-	//Method to calculate how many matches are needed
-	public static int calculation(int amountTeams){
+
+	// Method to calculate how many matches are needed
+	public static int calculation(int amountTeams) {
 
 		int matches = amountTeams * (amountTeams - 1) / 2;
 		return matches;
 
 	}
 
-	//Method to ensure the number of teams inputed are even
-	public static boolean isDivisible(int amountTeams, int x){
+	// Method to ensure the number of teams inputed are even
+	public static boolean isDivisible(int amountTeams, int x) {
 
-		if(amountTeams % x == 0)
+		if (amountTeams % x == 0)
 			return true;
 		return false;
 	}
 
-	//Method to figure out who plays who
-	public static String[][] matchUps(String [] teamNames){
+	// Method to figure out who plays who
+	public static String[][] matchUps(String[] teamNames) {
 		int counter = 0;
-		String [][] schedule = new String[2][calculation(amountTeams)];
-		for(int i = 0; i < amountTeams; i++){
-			for(int j = i + 1; j < amountTeams; j++){
-				schedule [0][counter] = teamNames[i];
-				schedule [1][counter] = teamNames[j];
+		String[][] schedule = new String[2][calculation(amountTeams)];
+		for (int i = 0; i < amountTeams; i++) {
+			for (int j = i + 1; j < amountTeams; j++) {
+				schedule[0][counter] = teamNames[i];
+				schedule[1][counter] = teamNames[j];
 				counter++;
 			}
 		}
@@ -75,19 +86,29 @@ public class TournamentPlanner {
 		return schedule;
 	}
 
-	//Method to organize who plays when
-	public static int nextMatch(String [][] matchups, String team1, String team2 ){
-		int counter = 0;
-		for( int i = 0; i < amountTeams; i++){
-			if (team1 == matchups [0][counter]  || team2 == matchups[0][counter] || team1 == matchups[1][counter] || team2 == matchups[1][counter]){
-				System.out.println(counter);
-			}else{
-				
+	// Method to return the other team in every match up
+	public static int nextMatch(String[][] matchups, ArrayList<String> teams, int start){
+
+		for (int i = start; i < matchups[0].length; i++) {
+			if (teams.get(i) == matchups[0][i] || teams.get(i) == matchups[0][i] || teams.get(i) == matchups[1][i]
+					|| teams.get(i) == matchups[1][i]) {
+
+			} else {
+				return i;
 			}
 		}
-		
-		
-		return
+
+		return -1;
 	}
-	
+
+	// Method to organize who plays when
+	public static int match(String [][] matchups){
+		ArrayList<String> teamsChosen = new ArrayList<String>();
+		int matchChosen = 0; 
+		
+		teamsChosen.add(matchups[0][matchChosen]);
+		teamsChosen.add(matchups[1][matchChosen]);
+		
+		return //i don't know what to return :)
+	}
 }
